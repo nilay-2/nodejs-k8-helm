4cbb988b2238e3f2fc8ad7d4befb47cb63d33d5d
nilay <nilay.pophalkar@acc.ltd>
2025-07-21 16:07:00 +0530

    added readiness and liveliness probe for healthcheck

--- index.js
+++ index.js
@@ -3,6 +3,14 @@
   import v2Router from './routes/v2.route'
   const app = express()
 
+  process.on('unhandledRejection', (err) => {
+    console.error('Unhandled Rejection:', err);
+  });
+
+  process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err);
+  });
+
   app.get('/', (req, res) => {
     res.status(200).json({message: "Successful healthcheck"})
   })
@@ -10,6 +18,6 @@
   app.use('/api/v2', v2Router)
 
 
-  app.listen(5000, () => {
+  app.listen(5000,'0.0.0.0', () => {
     console.log('Server is running on port 5000, hahahaha, welcome to helm charts last updated - 21 july 2025')
   })
