9dbecaf0141cbf7fedeb276c9930e4a5ac19d52e
nilay <nilay.pophalkar@acc.ltd>
2025-07-21 15:37:41 +0530

    added healthcheck apis for liveliness and readiness probe

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ import v1Router from './routes/v1.route'
 import v2Router from './routes/v2.route'
 const app = express()
 
+app.get('/', (req, res) => {
+  res.status(200).json({message: "Successful healthcheck"})
+})
 app.use('/api/v1', v1Router)
 app.use('/api/v2', v2Router)
+
 
 app.listen(5000, () => {
   console.log('Server is running on port 5000, hahahaha, welcome to helm charts last updated - 21 july 2025')
